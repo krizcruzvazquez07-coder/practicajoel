2d891cb9a046d82c5c7a6bacd25ca1ad80fa38f1
Cristian Joel Cruz Vazquez <krizcruzvazquez07@gmail.com>
2025-09-24 10:39:10 -0600

    HOLA JOEL CARGA EL ARCHIVO

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Pal reporte nomas</title>
+    <style>
+        body {
+            background-color: #1ed797ff; 
+            font-family: Arial, Helvetica, sans-serif;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+        }
+        h1 {
+            color: #0d47a1;
+            margin-top: 60px;
+        }
+        .tarjeta {
+            background-color: #888;
+            width: 300px;
+            margin: 30px auto;
+            padding: 15px;
+            border-radius: 10px;
+            box-shadow: 2px 2px 10px rgba(0,0,0,0.2);
+        }
+        .tarjeta p{
+            background-color: #1ed797ff;
+            padding: 6px;
+            border-radius: 5px;
+            font-family: monospace;
+            margin: 8px 0;
+        }
+    </style>
+</head>
+<body>
+    <h1>esta es la evidencia de que si agarro el proyecto, ola profe armando mendez morales </h1>
+    <h4>Cristian Joel Cruz Vazquez</h4>
+
+    <div class="tarjeta">
+        <b>comandos para la instalacion de laravel y creacion del proyecto</b>
+        <p>composer create-project laravel/laravel nombredetuproyecto</p>
+        <p>cd nombredetuproyecto</p>
+        <p>php artisan serve</p>
+        <p>php artisan migrate</p>
+        <p>te metes al link que te muestra en el comando: php artisan serve</p>
+    </div>
+</body>
+</html>
